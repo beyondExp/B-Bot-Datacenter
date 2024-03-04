34628bddb676607fdd8ad8da623ff88f92529e13
Lucas Bärlocher <lucas.baerlocher@olai.com>
2024-03-04 16:04:11 +0100

    adding find and create so that the owner is added to the object when we create also when find is called the user object if it is defined determine which Objects can be called

--- src/api/ability/controllers/ability.js
+++ src/api/ability/controllers/ability.js
@@ -4,6 +4,42 @@
  * ability controller
  */
 
+
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::ability.ability');
+module.exports = createCoreController('api::ability.ability', {
+
+  async create(ctx) {
+    if(user){
+      const user = ctx.state.user;
+      const role = ctx.state.user.role
+      if(role.name !== "Administrator"){
+        const task = await super.create(ctx);
+
+        const updated = await strapi.entityService.update("api::ability.ability", task.data.id, {
+          data: {
+            owner: user.id
+          }
+        });
+
+        return result;
+      }
+    } else{
+      console.log("admin Access")
+    }
+  },
+  async find(ctx){
+    if(typeof user !== "undefined") {
+
+      const user = ctx.state.user;
+
+      ctx.query.filters = {
+        ...(ctx.query.filters || {}),
+        owner: user.id
+      };
+
+    }
+    return super.find(ctx);
+  }
+
+});
